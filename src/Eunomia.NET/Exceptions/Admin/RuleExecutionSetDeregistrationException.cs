d7b158d6f1447dd735bf5f8dc24f1fa54d3b92af
Ramon Long <ramonjlongiii@gmail.com>
2016-02-20 16:25:05 -0500

    Adding RuleExecutionSetDeregistrationException().

---
+++ src/Eunomia.NET/Exceptions/Admin/RuleExecutionSetDeregistrationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eunomia.NET.Exceptions.Admin
+{
+    public class RuleExecutionSetDeregistrationException : Exception
+    {
+        public RuleExecutionSetDeregistrationException()
+        {
+            
+        }
+
+        public RuleExecutionSetDeregistrationException(string message)
+            :base(message)
+        {
+            
+        }
+
+        public RuleExecutionSetDeregistrationException(string message, Exception inner)
+            :base(message, inner)
+        {
+            
+        }
+    }
+}
