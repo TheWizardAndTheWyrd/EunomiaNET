ffab3a7c6c33c2668fbba7749acf5dc6aca62b44
Ramon Long <ramonjlongiii@gmail.com>
2016-02-20 16:23:15 -0500

    Adding RuleExecutionSetCreateException().

---
+++ src/Eunomia.NET/Exceptions/Admin/RuleExecutionSetCreateException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eunomia.NET.Exceptions.Admin
+{
+    public class RuleExecutionSetCreateException : Exception
+    {
+        public RuleExecutionSetCreateException()
+        {
+            
+        }
+
+        public RuleExecutionSetCreateException(string message)
+            : base(message)
+        {
+            
+        }
+
+        public RuleExecutionSetCreateException(string message, Exception inner)
+            : base(message, inner)
+        {
+            
+        }
+    }
+}
