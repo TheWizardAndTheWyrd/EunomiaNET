1efd215fd8fdbc6423bf06d0550540df540b329b
Ramon Long <ramonjlongiii@gmail.com>
2016-02-20 16:20:59 -0500

    Adding RuleAdministrationException.

---
+++ src/Eunomia.NET/Exceptions/Admin/RuleAdministrationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eunomia.NET.Exceptions.Admin
+{
+    public class RuleAdministrationException : Exception
+    {
+        public RuleAdministrationException()
+        {
+            
+        }
+
+        public RuleAdministrationException(string message)
+            :base(message)
+        {
+            
+        }
+
+        public RuleAdministrationException(string message, Exception inner)
+            :base(message, inner)
+        {
+            
+        }
+    }
+}
