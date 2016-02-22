ac09a71616ec84030baa8f16f35ac4627fd2ff0a
Ramon Long <ramonjlongiii@gmail.com>
2016-02-22 13:09:31 -0500

    Initial version of RuleServiceProvider.

---
+++ src/Eunomia.NET/Rules/RuleServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using Eunomia.NET.Util;
+using Eunomia.NET.Interfaces.Rules;
+using Eunomia.NET.Interfaces.Rules.Admin;
+
+namespace Eunomia.NET.Rules
+{
+    public abstract class RuleServiceProvider<T>
+    {
+        ClassLoader<T> ClassLoader { get; set; }
+
+        IRuleRuntime RuleRuntime { get; set; }
+
+        IRuleAdministrator RuleAdministrator { get; set; }
+
+        public virtual T CreateInstance(string assemblyName)
+        {
+            if (this.ClassLoader == null)
+            {
+                this.ClassLoader = new ClassLoader<T>(assemblyName);
+            }
+
+            return this.ClassLoader.Activate();
+        }
+    }
+}
---
+++ src/Eunomia.NET/Util/ClassLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace Eunomia.NET.Util
+{
+    public class ClassLoader<T>
+    {
+        public object ActivatedObject { get; private set; }
+
+        public string AssemblyName { get; private set; }
+
+        public Type ActivatedType { get; private set; }
+
+        public ClassLoader(string assemblyName)
+        {
+            AssemblyName = assemblyName;
+        }
+
+        public T Activate()
+        {
+            Assembly assembly;
+
+            try
+            {
+                assembly = Assembly.LoadFrom(AssemblyName);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Can't find Assembly: {AssemblyName}", e);
+            }
+
+            try
+            {
+                ActivatedType = assembly.GetType(typeof (T).ToString());
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Can't get ActivatedType: {typeof(T)}", e);
+            }
+
+            try
+            {
+                ActivatedObject = Activator.CreateInstance(ActivatedType);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to activate object of Type: {ActivatedType}", e);
+            }
+
+            return (T) ActivatedObject;
+        }
+    }
+}
