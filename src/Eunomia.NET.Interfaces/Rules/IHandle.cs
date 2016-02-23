8dc06e72c60bb04da69b52618bb9ccb4c703e510
Ramon Long <ramonjlongiii@gmail.com>
2016-02-23 01:36:38 -0500

    First implementation of the Handle class.

--- src/Eunomia.NET.Interfaces/Rules/IHandle.cs
+++ src/Eunomia.NET.Interfaces/Rules/IHandle.cs
@@ -1,5 +1,8 @@
 ﻿namespace Eunomia.NET.Interfaces.Rules
 {
+    /// <summary>
+    /// TODO: Determine what, if anything, needs to be in this interface and if this interface is actually needed.
+    /// </summary>
     public interface IHandle
     {
          
---
+++ src/Eunomia.NET/Rules/Handle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Eunomia.NET.Interfaces;
+using Eunomia.NET.Interfaces.Rules;
+
+namespace Eunomia.NET.Rules
+{
+    [Serializable]
+    public class Handle : IHandle
+    {
+        private object _mObject;
+        private IFact _mFact;
+
+        public object MObject
+        {
+            get { return _mObject; }
+            private set { _mObject = value; }
+        }
+
+        public IFact MFact
+        {
+            get { return _mFact; }
+            private set { _mFact = value; }
+        }
+
+        public Handle(IFact fact, object obj)
+        {
+            this._mFact = fact;
+            this._mObject = obj;
+        }
+
+        // TODO: Work on this and the complimentary != operator.
+        //public static bool operator == (Handle leftObject, IHandle rightObject)
+        //{
+        //    return ((Handle) leftObject == (rightObject as Handle));
+        //}
+
+        /// <summary>
+        /// This Equals method overrides the virtual method in the System.Object class.
+        /// 
+        /// There are two conditions whose truthiness must be evaluated:
+        /// 
+        /// 1.  Is the parameter object o an instance of the Type of MObject?
+        /// 
+        /// 2.  Is this.MObject == o.Object after casting o to <see cref="Handle"/>?
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public new bool Equals(object o)
+        {
+            return (o.GetType().IsInstanceOfType(this.MObject)) && (this.MObject == ((Handle) o).MObject);
+        }
+
+        public int HashCode()
+        {
+            return this.GetHashCode();
+        }
+    }
+}
