a2894954cf10b00d6fdb519623047ea1ffe72950
Ramon Long <ramonjlongiii@gmail.com>
2016-02-23 03:07:22 -0500

    Adding placeholder implementation of ObjectFilter : IObjectFilter.

---
+++ src/Eunomia.NET/Rules/ObjectFilter.cs
@@ -0,0 +1,17 @@
+﻿using Eunomia.NET.Interfaces.Rules;
+
+namespace Eunomia.NET.Rules
+{
+    public class ObjectFilter : IObjectFilter
+    {
+        public object Filter(object filter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
